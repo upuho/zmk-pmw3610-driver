a10b89e1888e7cd3e336342d4dc33f871bcc5a9c
upuho <67149771+upuho@users.noreply.github.com>
2025-06-01 23:50:29 +0900

    Update pmw3610.c

--- src/pmw3610.c
+++ src/pmw3610.c
@@ -630,6 +630,27 @@ static int pmw3610_report_data(const struct device *dev) {
     int16_t raw_y =
         TOINT16((buf[PMW3610_Y_L_POS] + ((buf[PMW3610_XY_H_POS] & 0x0F) << 8)), 12) / dividor;
 
+#ifdef CONFIG_PMW3610_ADJUSTABLE_MOUSESPEED
+    int16_t movement_size = abs(raw_x) + abs(raw_y);
+
+    float speed_multiplier = 1.0; //速度の倍率
+    if (movement_size > 60) {
+        speed_multiplier = 3.0;
+    }else if (movement_size > 30) {
+        speed_multiplier = 1.5;
+    }else if (movement_size > 10) {
+        speed_multiplier = 1.0;
+    }else if (movement_size > 5) {
+        speed_multiplier = 0.8;
+    }else if (movement_size > 1) {
+        speed_multiplier = 0.6;
+    }
+
+    raw_x = raw_x * speed_multiplier;
+    raw_y = raw_y * speed_multiplier;
+
+#endif
+
     int16_t x;
     int16_t y;
 
